d5ded1949c2675c4d1e24bf9bf9a48ec33de5b31
steventan8931 <85856246+steventan8931@users.noreply.github.com>
2022-05-02 11:32:32 +1200

    Start

---
+++ Source/OneStarPrison/OneStarPrisonGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OneStarPrisonGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOneStarPrisonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOneStarPrisonGameMode();
+};
+
+
+
