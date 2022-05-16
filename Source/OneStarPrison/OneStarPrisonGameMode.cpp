2ece704a9d717b9699e13ee4cdb4d2243c71beec
Steven Tan <steven.tan8931@gmail.com>
2022-05-16 19:53:51 +1200

    Update OneStarPrisonGameMode.cpp

--- Source/OneStarPrison/OneStarPrisonGameMode.cpp
+++ Source/OneStarPrison/OneStarPrisonGameMode.cpp
@@ -6,7 +6,11 @@
 
 AOneStarPrisonGameMode::AOneStarPrisonGameMode()
 {
+<<<<<<< Updated upstream
 	// set default pawn class to our Blueprinted character
+=======
+	//// set default pawn class to our Blueprinted character
+>>>>>>> Stashed changes
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	//if (PlayerPawnBPClass.Class != NULL)
 	//{
