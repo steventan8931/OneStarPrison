97a96b0df695c1a001957c10ba7734b335061183
Steven Tan <steven.tan8931@gmail.com>
2022-06-17 11:59:05 +1200

    moving platform on sectionD, checkpoint debugger

---
+++ Source/OneStarPrison/Public/MovingPlatform.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class ONESTARPRISON_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere)
+		class UBoxComponent* BoxCollision;
+
+	UPROPERTY(EditAnywhere)
+		class ACrankliftPlatform* Platform;
+
+	UPROPERTY(EditAnywhere)
+		FVector OpenPosition = FVector(0, 0, 0);
+
+	UPROPERTY(EditAnywhere)
+		FVector ClosedPosition = FVector(0, 0, 0);
+
+	UPROPERTY()
+		bool IsMoving = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float MoveSpeed = 20.0f;
+
+	UPROPERTY(VisibleAnywhere)
+		class APlayerCharacter* OverlappingPlayer = nullptr;
+
+	//HUD Class to add to viewport
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<class UUserWidget> HUDWidgetClass;
+
+	UPROPERTY(VisibleAnywhere)
+		class UUserWidget* CurrentWidget;
+
+	void InteractPopUp();
+
+	//Overlap Functions
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//Overlap Functions
+	UFUNCTION()
+		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+};
