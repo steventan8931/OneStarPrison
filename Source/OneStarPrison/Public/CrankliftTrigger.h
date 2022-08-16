e3b0a914f1c5a0bd92f111fcdcfdee59132c5471
Steven Tan <steven.tan8931@gmail.com>
2022-08-16 23:14:58 +1200

    commenting code for code review

--- Source/OneStarPrison/Public/CrankliftTrigger.h
+++ Source/OneStarPrison/Public/CrankliftTrigger.h
@@ -22,56 +22,6 @@ protected:
 	//Replication
 	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	UPROPERTY(EditAnywhere)
-		USoundBase* MovingSound;
-
-	UPROPERTY(EditAnywhere, Replicated)
-		UAudioComponent* AudioComponent;
-
-	UFUNCTION(Server, Unreliable)
-		void ServerPlaySound(bool _IsPaused);
-	UFUNCTION(NetMulticast, Unreliable)
-		void ClientPlaySound(bool _IsPaused);
-
-	UPROPERTY(VisibleAnywhere)
-		class UStaticMeshComponent* Mesh;
-
-	UPROPERTY(VisibleAnywhere)
-		class UStaticMeshComponent* MovableMesh;
-
-	UPROPERTY(EditAnywhere, Replicated)
-		FRotator HandleOpenRotation = FRotator(0, 0, -50);
-
-	UPROPERTY(EditAnywhere, Replicated)
-		FRotator HandleClosedRotation = FRotator(0, 0, 50);
-
-	UPROPERTY(VisibleAnywhere)
-		class UBoxComponent* BoxCollision;
-
-	UPROPERTY(EditAnywhere, Replicated)
-		class ACrankliftPlatform* Platform;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
-		float MaxHeight = 0;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
-		float MinHeight = 0;
-
-	UPROPERTY(EditAnywhere, Replicated)
-		bool IsMovingUp = false;
-
-	float cacheDeltaTime = 0.0f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		float MoveSpeed = 20.0f;
-
-	UPROPERTY(VisibleAnywhere)
-		class APlayerCharacter* OverlappingPlayer = nullptr;
-
 	//Overlap Functions
 	UFUNCTION()
 		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
@@ -79,6 +29,56 @@ public:
 	//Overlap Functions
 	UFUNCTION()
 		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//Components
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent* MovableMesh;
+	UPROPERTY(VisibleAnywhere)
+		class UBoxComponent* BoxCollision;
+
+	//Audio
+	UPROPERTY(EditAnywhere)
+		USoundBase* MovingSound;
+	UPROPERTY(EditAnywhere, Replicated)
+		UAudioComponent* AudioComponent;
+
+	//Sound Replication
+	UFUNCTION(Server, Unreliable)
+		void ServerPlaySound(bool _IsPaused);
+	UFUNCTION(NetMulticast, Unreliable)
+		void ClientPlaySound(bool _IsPaused);
+
+	//Open and closed rotations for the handle
+	UPROPERTY(EditAnywhere, Replicated)
+		FRotator HandleOpenRotation = FRotator(0, 0, -50);
+	UPROPERTY(EditAnywhere, Replicated)
+		FRotator HandleClosedRotation = FRotator(0, 0, 50);
+
+	//Max and Min heights for the cranklift
+	UPROPERTY(EditAnywhere, Replicated)
+		float MaxHeight = 0;
+	UPROPERTY(EditAnywhere, Replicated)
+		float MinHeight = 0;
+
+	//Linked Platform
+	UPROPERTY(EditAnywhere, Replicated)
+		class ACrankliftPlatform* Platform;
+
+	//Movement
+	UPROPERTY(EditAnywhere, Replicated)
+		bool IsMovingUp = false;
+	UPROPERTY(EditAnywhere)
+		float MoveSpeed = 20.0f;
+
+	//Player interaction
+	UPROPERTY(VisibleAnywhere)
+		class APlayerCharacter* OverlappingPlayer = nullptr;
 
 
 };
