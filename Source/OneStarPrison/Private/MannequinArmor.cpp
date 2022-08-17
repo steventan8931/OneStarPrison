ce4c3da3aa16d03fb9087707cee859d1538add23
Steven Tan <steven.tan8931@gmail.com>
2022-08-17 18:40:32 +1200

    fix dressing room mannequin things dissapearing (reset if on ground for >10 s) (wall+floor collision boxes added)

--- Source/OneStarPrison/Private/MannequinArmor.cpp
+++ Source/OneStarPrison/Private/MannequinArmor.cpp
@@ -3,3 +3,56 @@
 
 #include "MannequinArmor.h"
 
+// Called when the game starts or when spawned
+void AMannequinArmor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	cacheTransform = GetActorTransform();
+
+	ParentActor = GetParentActor();
+}
+
+// Called every frame
+void AMannequinArmor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (OnDisplay)
+	{
+		if (Player)
+		{
+			TakenFromDisplay = true;
+		}
+		else
+		{
+			if (TakenFromDisplay)
+			{
+				if (!IsInAir)
+				{
+					Timer += DeltaTime;
+				}
+			}
+		}
+
+
+		if (Timer > TimeBeforeItRespawns)
+		{
+			TakenFromDisplay = false;
+			if (ParentActor)
+			{
+				AttachToActor(ParentActor, FAttachmentTransformRules::SnapToTargetIncludingScale);
+			}
+			SetActorTransform(cacheTransform);
+			Mesh->SetSimulatePhysics(false);
+
+			if (ReturnToDisplaySound)
+			{
+				UGameplayStatics::PlaySoundAtLocation(GetWorld(), ReturnToDisplaySound, GetActorLocation());
+			}
+
+			Timer = 0.0f;
+		}
+
+	}
+}
