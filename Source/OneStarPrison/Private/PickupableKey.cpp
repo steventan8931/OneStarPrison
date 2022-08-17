b35b61850f91b25f57a63fde343eaa3f5f667eee
Steven Tan <steven.tan8931@gmail.com>
2022-08-17 18:13:33 +1200

    make the keys teleports back if missing (in the dressing room)

--- Source/OneStarPrison/Private/PickupableKey.cpp
+++ Source/OneStarPrison/Private/PickupableKey.cpp
@@ -2,3 +2,57 @@
 
 
 #include "PickupableKey.h"
+
+// Called when the game starts or when spawned
+void APickupableKey::BeginPlay()
+{
+	Super::BeginPlay();
+
+	cacheTransform = GetActorTransform();
+
+	ParentActor = GetParentActor();
+}
+
+// Called every frame
+void APickupableKey::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (OnDisplay)
+	{
+		if (Player)
+		{
+			TakenFromDisplay = true;
+		}
+		else
+		{
+			if (TakenFromDisplay)
+			{
+				if (!IsInAir)
+				{
+					Timer += DeltaTime;
+				}
+			}
+		}
+
+
+		if (Timer > TimeBeforeItRespawns)
+		{
+			TakenFromDisplay = false;
+			if (ParentActor)
+			{
+				AttachToActor(ParentActor, FAttachmentTransformRules::SnapToTargetIncludingScale);
+			}
+			SetActorTransform(cacheTransform);
+			Mesh->SetSimulatePhysics(false);
+
+			if (ReturnToDisplaySound)
+			{
+				UGameplayStatics::PlaySoundAtLocation(GetWorld(), ReturnToDisplaySound, GetActorLocation());
+			}
+
+			Timer = 0.0f;
+		}
+
+	}
+}
--- Source/OneStarPrison/Public/PickupableKey.h
+++ Source/OneStarPrison/Public/PickupableKey.h
@@ -13,8 +13,31 @@ UCLASS()
 class ONESTARPRISON_API APickupableKey : public APickupable
 {
 	GENERATED_BODY()
-	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 public:
 	UPROPERTY(EditAnywhere)
 		int KeyCode = 0;
+
+	//For pickupable display (book)
+	UPROPERTY(EditAnywhere)
+		bool OnDisplay = false;
+
+	bool TakenFromDisplay = false;
+
+	AActor* ParentActor;
+
+	FTransform cacheTransform;
+
+	float Timer = 0.0f;
+
+	UPROPERTY(EditAnywhere)
+		float TimeBeforeItRespawns = 5.0f;
+
+	UPROPERTY(EditAnywhere)
+		USoundBase* ReturnToDisplaySound;
 };
