2561e5e8a5a50f7ee3981d9090223723a6d28c25
Steven Tan <steven.tan8931@gmail.com>
2022-08-28 23:50:19 +1200

    commented + cleaned up player code

--- Source/OneStarPrison/Public/PlayerCharacter.h
+++ Source/OneStarPrison/Public/PlayerCharacter.h
@@ -90,37 +90,15 @@ private:
 		class UUserWidget* CurrentPickupWidget;
 	UPROPERTY(Replicated)
 		class UUserWidget* CurrentInteractWidget;
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
-	//Checks whether the player can move or not (used with holing levers)
-	UPROPERTY(EditAnywhere, Replicated, BlueprintReadWrite)
-		bool CanMove = true;
-	
-	//Cached armlength for when it will be changed for the maze
-	UPROPERTY(BlueprintReadWrite)
-	float cacheArmLength = 0.0f;
-
-	//When the player presses the interact button
-	void Interact();
-	//Call function on the server
+	//When the player presses the interact button on the server
 	UFUNCTION(Server, Reliable)
-		void RPCInteract();
-	//When the player releases the interact button
-	void StopInteract();
-	//Call function on the server
+		void Interact();
+	//When the player releases the interact button on the server
 	UFUNCTION(Server, Reliable)
-		void RPCStopInteract();
-	//Whetther the player is able to interact (Interact prompt shows up)
-	UPROPERTY(Replicated)
-	bool CanInteract = false;
-	//Whether the player is interacting (holding down / clicked interact button)
-	UPROPERTY(BlueprintReadWrite,Replicated)
-	bool IsInteracting = false;
+		void StopInteract();
 
+	//Interact UI
 	//Have the server to check for the interact
 	UFUNCTION(Server, Reliable)
 		void ServerCheckInteract();
@@ -136,10 +114,8 @@ public:
 	//Call Pick up on the client
 	UFUNCTION(NetMulticast, Unreliable)
 		void ClientRPCPickupAndDrop(APickupable* _Pickup);
-	//Checks if the player is picking up (for the animation)
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
-		bool IsPickingUp = false;
 
+	//Pick Up UI
 	//Have the server to check for the pick up
 	UFUNCTION(Server, Reliable)
 		void ServerCheckPickup();
@@ -148,20 +124,81 @@ public:
 		void CheckPickup();
 
 	//Throw Picked up item
-	void Throw();
 	UFUNCTION(Server, Reliable)
-		void ServerRPCThrow();
+		void Throw();
+	//Throws item from the player
 	UFUNCTION(NetMulticast, Reliable)
 		void ClientRPCThrow();
+	//Shows throw UI on throwing player
 	UFUNCTION(Client, Unreliable)
 		void ClientShowThrowWidget();
 
+	//caches the throw velocity from the predicted path to the actual path
+	FVector cacheVelocity;
 
+	//Throw Projectile Path Rendering
+	UPROPERTY(VisibleAnywhere)
+		class USplineComponent* SplineComponent;
+	UPROPERTY(VisibleAnywhere)
+		TArray<class USplineMeshComponent*> SplineComponentArray;
+	UPROPERTY(EditAnywhere)
+		UStaticMesh* SplineMesh;
+	UPROPERTY(EditAnywhere)
+		class UMaterialInstance* SplineMeshMaterial;
 
+	//Renders the projectile path of the throwing item
+	void ShowProjectilePath(float _DeltaTime);
+
+	//The scale of normal speed vs crouch speed
+	UPROPERTY(EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+		float CrouchSpeedScale = 0.25f;
+	//Makes the player crouch
+	UFUNCTION(Server, Reliable)
+		void StartCrouching();
+	//Makes the player uncrouch
+	UFUNCTION(Server, Reliable)
+		void StopCrouching();
+
+	//The speed of the player when climbing
+	UPROPERTY(EditAnywhere, Replicated)
+		float ClimbSpeed = 50.0f;
+	//Updates the player variables for when they are/not climbing
+	UFUNCTION(NetMulticast, Reliable)
+		void CheckClimbing();
+
+	//Checks if dead then respawn player
+	void CheckDeath(float _DeltaTime);
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	//Checks whether the player can move or not (used with holing levers)
+	UPROPERTY(EditAnywhere, Replicated, BlueprintReadWrite)
+		bool CanMove = true;
+	
+	//Interaction
+	//Interact Type
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TEnumAsByte<EInteractType> InteractType;
+	//Whetther the player is able to interact (Interact prompt shows up)
+	UPROPERTY(Replicated)
+	bool CanInteract = false;
+	//Whether the player is interacting (holding down / clicked interact button)
+	UPROPERTY(BlueprintReadWrite,Replicated)
+	bool IsInteracting = false;
+
+	//Pickup
+	//Checks if the player is picking up (for the animation)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+		bool IsPickingUp = false;
+
+	//Throwing
 	//Check if Throw Key is being held down
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
 	bool IsHoldingDownThrow = false;
-
 	//Current Throw Power
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	float ThrowPowerScale = 0;
@@ -169,22 +206,10 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float MaxThrowPower = 150.0f;
 
-	UPROPERTY(BlueprintReadWrite)
-		FVector cacheVelocity;
-
-	//Throw Rendering
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-		class USplineComponent* SplineComponent;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-		TArray<class USplineMeshComponent*> SplineComponentArray;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		UStaticMesh* SplineMesh;
-	UPROPERTY(EditAnywhere)
-		class UMaterialInstance* SplineMeshMaterial;
-
-	//Punch Interact delay
+	//Punch Interact timer
 	UPROPERTY(BlueprintReadWrite, Replicated)
 		float PunchTimer = 0.0f;
+	//Punch Interact delay
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
 		float PunchDelay = 0.5f;
 
@@ -192,50 +217,45 @@ public:
 	UPROPERTY(VisibleAnywhere, Replicated, BlueprintReadWrite)
 		class APickupable* PickedUpItem = nullptr;
 
-	//Death
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		FVector RespawnCheckpoint = FVector(0, 0, 0);
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		bool IsDead = false;
+	//Crouching/Sneaking
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
-		int HitByWallCount = 0;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		float DeathTimer = 1.0f;
-	float DeathTimerCounter = 0.0f;
-	//Checks if dead then respawn player
-	void CheckDeath(float _DeltaTime);
+		bool IsCrouching = false;
 
 	//Pushing
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	//Whether the player is in the box collision and able to push or not
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		bool CanPush = false;
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	//Whether the player is pushing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		bool IsPushing = false;
 
-
-	//Crouching/Sneaking
-	UPROPERTY(EditAnywhere, BlueprintReadWrite,Replicated)
-		bool IsCrouching = false;
-	UPROPERTY(EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
-		float CrouchSpeedScale = 0.25f;
-	UFUNCTION(Server, Reliable)
-	void StartCrouching();
-	UFUNCTION(Server, Reliable)
-	void StopCrouching();
-
 	//Climbing
+	//Checks whether the player is climbing
 	UPROPERTY(BlueprintReadWrite, Replicated)
 		bool IsClimbing = false;
-	UPROPERTY(EditAnywhere, Replicated)
-		float ClimbSpeed = 50.0f;
-	UFUNCTION(NetMulticast, Reliable)
-	void CheckClimbing();
 
-	//Heavy item
-	bool CheckHeavyItem();
-	bool IsHoldingHeavyItem = false;
-
-	//Interact Type
+	//Death
+	//The location the player is respawn to 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		FVector RespawnCheckpoint = FVector(0, 0, 0);
+	//Check whether the player is dead or not
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		TEnumAsByte<EInteractType> InteractType;
+		bool IsDead = false;
+	//Used by the death walls to check it has been hit by both walls
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+		int HitByWallCount = 0;
+	//Delay before the player is respawned after dying
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float DeathTimer = 1.0f;
+	//Counter for the death timer
+	float DeathTimerCounter = 0.0f;
 
+	//Cached armlength for when it will be changed for the maze
+	UPROPERTY(BlueprintReadWrite)
+		float cacheArmLength = 0.0f;
+
+	//Returns whether the player is holding an heavy item
+	bool CheckHeavyItem();
+	//Checks whether the player is holding an heavy item
+	bool IsHoldingHeavyItem = false;
 };
