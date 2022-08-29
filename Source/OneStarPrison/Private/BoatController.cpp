986925ff99cc3f6fb8c2519e6ff3c243835a151d
steventan8931 <steven.tan8931@gmail.com>
2022-08-29 12:13:13 +1200

    moving boat + boat controls

---
+++ Source/OneStarPrison/Private/BoatController.cpp
@@ -0,0 +1,143 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BoatController.h"
+#include "Components/BoxComponent.h"
+#include "PickupableBook.h"
+#include "PlayerCharacter.h"
+
+#include <Runtime/Engine/Public/Net/UnrealNetwork.h>
+
+#include "ControllableBoat.h"
+
+// Sets default values
+ABoatController::ABoatController()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+	Mesh->SetCollisionProfileName(TEXT("Trigger"));
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetBoxExtent(FVector(100, 100, 100));
+	BoxCollision->SetCollisionProfileName(TEXT("Trigger"));
+	BoxCollision->SetupAttachment(RootComponent);
+
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ABoatController::OnOverlapBegin);
+	BoxCollision->OnComponentEndOverlap.AddDynamic(this, &ABoatController::OnOverlapEnd);
+}
+
+// Called when the game starts or when spawned
+void ABoatController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABoatController::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ABoatController, InteractDelay);
+	DOREPLIFETIME(ABoatController, InteractTimer);
+
+	DOREPLIFETIME(ABoatController, RotationScale);
+}
+
+// Called every frame
+void ABoatController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (Boat)
+	{
+		if (!Boat->IsMoving)
+		{
+			return;
+		}
+
+		InteractTimer += DeltaTime;
+
+		if (OverlappingPlayer)
+		{
+			//If the player's last punch has been longer than the punch delay
+			if (InteractTimer >= InteractDelay)
+			{
+				OverlappingPlayer->CanInteract = true;
+
+				if (OverlappingPlayer->CanInteract)
+				{
+					//If the player is interacting, update the health and reset the players punch/interact timer
+					if (OverlappingPlayer->IsInteracting)
+					{
+						if (HasAuthority())
+						{
+							FRotator rot = FRotator(0, RotationScale, 0);
+							FRotator newRot = FMath::Lerp(Boat->GetActorRotation(), Boat->GetActorRotation() + rot, DeltaTime * 2);
+							Boat->SetActorRotation(newRot);
+							//Boat->AddActorWorldRotation(newRot);				
+						}
+
+						OverlappingPlayer->CanInteract = false;
+						InteractTimer = 0.0f;
+					}
+				}
+			}
+			else
+			{
+				//Don't allow the overlapping player to interact
+				OverlappingPlayer->IsInteracting = false;
+				OverlappingPlayer->CanInteract = false;
+			}
+		}
+	}
+
+}
+
+void ABoatController::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this))
+	{
+		APlayerCharacter* playerActor = Cast<APlayerCharacter>(OtherActor);
+
+		//If the overlapping actor is a player
+		if (playerActor)
+		{
+			//If there isn't already an overlapping player
+			if (!OverlappingPlayer)
+			{
+				//Set the current player to the overlapping player
+				OverlappingPlayer = playerActor;
+			}
+		}
+
+	}
+}
+
+void ABoatController::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+	if (OtherActor && (OtherActor != this))
+	{
+		APlayerCharacter* playerActor = Cast<APlayerCharacter>(OtherActor);
+
+		//If the overlapping actor is a player
+		if (playerActor)
+		{
+			//If an overlapping player exists
+			if (OverlappingPlayer)
+			{
+				//If the current player is the overlapping player
+				if (playerActor == OverlappingPlayer)
+				{
+					//Make the overlapping player can't interact 
+					OverlappingPlayer->CanInteract = false;
+					OverlappingPlayer = nullptr;
+				}
+			}
+		}
+	}
+}
+
---
+++ Source/OneStarPrison/Public/ControllableBoat.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ControllableBoat.generated.h"
+
+UCLASS()
+class ONESTARPRISON_API AControllableBoat : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AControllableBoat();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Replication
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
+
+	//Overlap Functions
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//Overlap Functions
+	UFUNCTION()
+		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//Components
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere)
+		class UBoxComponent* BoxCollision;
+
+	//Sound that plays when the wheel is inserted
+	UPROPERTY(EditAnywhere)
+		USoundBase* InsertSound;
+
+	UPROPERTY(EditAnywhere)
+		float Speed = 100.0f;
+
+	//Player Interaction
+	UPROPERTY(VisibleAnywhere)
+		class APlayerCharacter* OverlappingPlayer = nullptr;
+public:
+
+
+	UPROPERTY(EditAnywhere, Replicated)
+		bool IsMoving = false;
+};
